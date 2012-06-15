a67efe83f682fb11bf753c39ff61ad8cf611bed6
Jochen Szostek <jochen@prefabsoft.com>
2012-06-15 15:07:02 +0200

    Log entity fix

--- dev/money.prefabsoft.com/src/main/java/com/prefabsoft/log/Log.java
+++ dev/money.prefabsoft.com/src/main/java/com/prefabsoft/log/Log.java
@@ -1,44 +1,42 @@
 package com.prefabsoft.log;
 
-import org.springframework.roo.addon.entity.RooEntity;
-import org.springframework.roo.addon.javabean.RooJavaBean;
-import org.springframework.roo.addon.tostring.RooToString;
 import java.util.Date;
-
-import javax.persistence.Column;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.json.RooJson;
+import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
-@RooEntity
+@RooJpaActiveRecord
 @RooJson
 public class Log {
-
-    public Log() {
-		super();
+	
+	public Log() {
 	}
-
+	
 	public Log(String logLevel, String logMessage) {
 		super();
 		this.logLevel = logLevel;
 		this.logMessage = logMessage;
 	}
 
-	private String logLevel;
-	
-	@Column(length=2048)
+    private String logLevel;
+
+    @Size(min = 4096)
     private String logMessage;
 
     private Boolean active;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(style = "S-")
+    @DateTimeFormat(style = "M-")
     private Date createDate;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(style = "S-")
+    @DateTimeFormat(style = "M-")
     private Date updateDate;
 }
--- dev/money.prefabsoft.com/src/main/java/com/prefabsoft/web/HttpInfoController.java
+++ dev/money.prefabsoft.com/src/main/java/com/prefabsoft/web/HttpInfoController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.prefabsoft.web.util.LogUtilities;
+
 @Controller
 public class HttpInfoController {
 
